8fd2623eaba940290ac7e407a54d211a72288ea8
tmvlo <142369701+tmvlo@users.noreply.github.com>
2023-08-29 18:59:28 -0400

    Create slider-logos.js

---
+++ slider-logos.js
@@ -0,0 +1,16 @@
+//Slick Marquee Init
+$('.slick.marquee').slick({
+  speed: 10000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  centerMode: true,
+  cssEase: 'linear',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  pauseOnHover: false,
+  infinite: true,
+  initialSlide: 1,
+  arrows: false,
+  buttons: false
+});
