0d2307494ed953f73ac78382222038e410022264
tmvlo <142369701+tmvlo@users.noreply.github.com>
2023-08-29 19:05:25 -0400

    Update slider-logos.js

--- slider-logos.js
+++ slider-logos.js
@@ -1,16 +1,19 @@
 //Slick Marquee Init
-$('.slick.marquee').slick({
-  speed: 10000,
-  autoplay: true,
-  autoplaySpeed: 0,
-  centerMode: true,
-  cssEase: 'linear',
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  variableWidth: true,
-  pauseOnHover: false,
-  infinite: true,
-  initialSlide: 1,
-  arrows: false,
-  buttons: false
-});
+(async function (event) {
+    $('.slick.marquee').slick({
+      speed: 10000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      centerMode: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      pauseOnHover: false,
+      infinite: true,
+      initialSlide: 1,
+      arrows: false,
+      buttons: false
+    });
+})();
+
